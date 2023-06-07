6d15cdfc09a78e51fab2c3985c17c3fc0020b0cf
SheikSalauddinBiswas <salauddinbiswas7@gmail.com>
2023-06-07 11:06:37 +0600

    gd

--- resources/views/user/dashboard/uhome.blade.php
+++ resources/views/user/dashboard/uhome.blade.php
@@ -1,39 +1,24 @@
-<!doctype html>
-<html lang="en">
-  <head>
-  	<title>User Dashboard</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('user.dashboard.dashboard')
 
-	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
-
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
-
-	</head>
-	<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+@section('content')
+  <br/>
+    <div class="card" style="width: 18rem;">
+      <img src="{{asset('public/images/book.jpg')}}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">General Diary</h5>
+        <p class="card-text">is an important process that allows individuals to record a complaint or report an incident to the police in Bangladesh.</p>
+        <a href="{{url('/general-diary')}}" class="btn btn-primary">Make a GD</a>
+      </div>
     </div>
-  </div>
-</nav>
-	<section class="ftco-section">
-      
+    
+  
+  
+  
+  
+  
+  
+  
+  
+@endsection
 		
-    </section>
-
-
-	</body>
-</html>
 
