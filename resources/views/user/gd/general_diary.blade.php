aad65997b84dfc6a3b792915a14df0007aa03e06
SheikSalauddinBiswas <salauddinbiswas7@gmail.com>
2023-06-09 23:47:58 +0600

    route problem

--- resources/views/user/gd/general_diary.blade.php
+++ resources/views/user/gd/general_diary.blade.php
@@ -17,14 +17,22 @@
                             <input class="input--style-1" type="text" placeholder="Name" name="name" required>
                         </div>
                         <div class="input-group">
-                            <input class="input--style-1" type="text" placeholder="Father Name" name="fname" required>
+                            <input class="input--style-1" type="text" placeholder="Father Name" name="fname" >
                         </div>
                         <div class="input-group">
                             <input class="input--style-1" type="email" placeholder="E-mail" name="email" required>
                         </div>
-                        
-                        <div class="input-group">
-                            <input class="input--style-1" type="number" placeholder="Mobile" name="mobile" required>
+                        <div class="row row-space d-flex">
+                            <div class="col-6">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="number" placeholder="Mobile" name="mobile" required>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="number" placeholder="Eergency Mobile" name="emobile" required>
+                                </div>
+                            </div>
                         </div>
                         <div class="row row-space">
                             <div class="col-2">
@@ -36,22 +44,22 @@
                         <div class="row row-space d-flex">
                             <div class="col-4">
                                 <div class="input-group">
-                                    <input class="input--style-1" type="text" placeholder="City" name="nid_num" required>
+                                    <input class="input--style-1" type="text" placeholder="City" name="city" required>
                                 </div>
                             </div>
                             <div class="col-4">
                                 <div class="input-group">
-                                    <input class="input--style-1" type="text" placeholder="Union" name="nid_num" required>
+                                    <input class="input--style-1" type="text" placeholder="Union" name="union" required>
                                 </div>
                             </div>
                             <div class="col-4">
                                 <div class="input-group">
-                                    <input class="input--style-1" type="text" placeholder="Thana" name="nid_num" required>
+                                    <input class="input--style-1" type="text" placeholder="Thana" name="thana" required>
                                 </div>
                             </div>
                             <div class="col-4">
                                 <div class="input-group">
-                                    <input class="input--style-1" type="text" placeholder="Area" name="nid_num" required>
+                                    <input class="input--style-1" type="text" placeholder="Area" name="area" required>
                                 </div>
                             </div>
                         </div>
@@ -62,6 +70,77 @@
                                 </div>
                             </div>
                         </div>
+                        <h3><p class="text-secondary">Crime Data :</p></h3>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="text" placeholder="Crime Type" name="crime_type" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space d-flex">
+                            <label for="exampleFormControlTextarea1">Crime Location</label>
+                            <div class="col-4">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="text" placeholder="City" name="c_city" required>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="text" placeholder="Union" name="c_union" required>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="text" placeholder="Thana" name="c_thana" required>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="text" placeholder="Area" name="c_area" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <label for="exampleFormControlTextarea1">Crime Statement  :</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" name="statement" rows="3"></textarea>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-3">
+                                <div class="input-group">
+                                    <label for="exampleFormControlTextarea1">Time Of Incident  :</label>
+                                    <input class="input--style-1" type="time"  name="crime_type" required>
+                                </div>
+                            </div>
+                        </div> 
+                        <h3><p class="text-secondary">Identification Data :</p></h3>
+                        <div class="row row-space d-flex">
+                            <div class="col-6">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="text" placeholder="Color Of The Product" name="color_product" required>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                    <div class="input-group">
+                                        <input class="input--style-1" type="file" placeholder="Other Document of Product" name="product_document" required>
+                                    </div>
+                            </div>
+                        </div> 
+                         <div class="row row-space d-flex">
+                            <div class="col-6">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="text" placeholder="Identifier Data" name="identifier_data" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                                <div class="input-group">
+                                    <label for="exampleFormControlTextarea1">Additional Data Of Crime  :</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" name="addi_data_crime" rows="3"></textarea>
+                                </div>
+                        </div>
+
+
                         <div class="p-t-20">
                             <button class="btn btn--radius btn--green" type="submit">Submit</button>
                         </div>
